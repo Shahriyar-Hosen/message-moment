517e28b62ffc6d237fb941bfd1a8b936387d9cf0
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-05-26 19:40:25 +0600

    color added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#494AF8",
+        dark: "#363C4F",
+        "dark-gray": "#555555",
+        "light-gray": "#CCCCCC",
       },
     },
   },
