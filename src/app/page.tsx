50e809014f75d141a2a474d95d9f7e401c27bdbc
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-05-28 20:42:15 +0600

    typography added done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Note } from "@/components/typography/note";
 import { P } from "@/components/typography/paragraph";
 import { SubTitle } from "@/components/typography/subtitle";
+import { Title } from "@/components/typography/title";
 import { FC } from "react";
 
 const Home = () => {
@@ -13,26 +14,43 @@ const Home = () => {
       {children}
     </div>
   );
+
   return (
     <div className="flex justify-center items-center gap-5 h-screen flex-col">
-      <Typography title="Note">
-        <Note>N1 N1 N1 n1 n1 </Note>
-        <Note variant="N2">N2 N2 N2 n2 n2 </Note>
+      <Typography title="Title">
+        <Title>Title h1 Title h1 Title h1 Title h1 Title h1 </Title>
+        <Title variant="H2">Title h2 Title h2 Title h2 Title h2 Title h2</Title>
       </Typography>
+      <hr className="h-1 w-full bg-gray-600" />
+      <Typography title="SubTitle">
+        <SubTitle>
+          SubTitle 3 SubTitle 3 SubTitle 3 SubTitle 3 SubTitle 3
+        </SubTitle>
+        <SubTitle variant="H4">
+          SubTitle 4 SubTitle 4 SubTitle 4 SubTitle 4 SubTitle 4
+        </SubTitle>
+        <SubTitle variant="H5">
+          SubTitle 5 SubTitle 5 SubTitle 5 SubTitle 5 SubTitle 5
+        </SubTitle>
+        <SubTitle variant="H6">
+          SubTitle 6 SubTitle 6 SubTitle 6 SubTitle 6 SubTitle 6
+        </SubTitle>
+        <SubTitle variant="H7">
+          SubTitle 7 SubTitle 7 SubTitle 7 SubTitle 7 SubTitle 7
+        </SubTitle>
+      </Typography>
+      <hr className="h-1 w-full bg-gray-600" />
       <Typography title="Paragraph">
         <P>P1 P1 P1 p1 p1 </P>
         <P variant="P2">P2 P2 P2 p2 p2 </P>
         <P variant="P3">P3 P3 P3 p3 p3 </P>
       </Typography>
-      <Typography title="SubTitle">
-        <SubTitle>SubTitle1 SubTitle1 SubTitle1 SubTitle1 SubTitle1 </SubTitle>
-        <SubTitle variant="H4">
-          SubTitle2 SubTitle2 SubTitle2 SubTitle2 SubTitle2
-        </SubTitle>
-        <SubTitle variant="H5">
-          SubTitle3 SubTitle3 SubTitle3 SubTitle3 SubTitle3
-        </SubTitle>
+      <hr className="h-1 w-full bg-gray-600" />
+      <Typography title="Note">
+        <Note>N1 N1 N1 n1 n1 </Note>
+        <Note variant="N2">N2 N2 N2 n2 n2 </Note>
       </Typography>
+      <hr className="h-1 w-full bg-gray-600" />
     </div>
   );
 };
--- src/components/typography/title/h1.tsx
+++
@@ -1,3 +0,0 @@
-export const H1 = () => {
-  return <div>H1</div>;
-};
