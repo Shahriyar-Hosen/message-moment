712da2868fee534b8f123dbb96c619c8932490b9
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-05-28 18:24:15 +0600

    paragraph added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Note } from "@/components/typography/note";
+import { P } from "@/components/typography/paragraph";
 import { FC } from "react";
 
 const Home = () => {
@@ -6,7 +7,7 @@ const Home = () => {
     title,
     children,
   }) => (
-    <div className="space-y-2.5">
+    <div className="space-y-2.5 w-fit">
       <h1 className="text-primary text-5xl font-extrabold">{title} </h1>
       {children}
     </div>
@@ -16,6 +17,11 @@ const Home = () => {
       <Typography title="Note">
         <Note>N1 N1 N1 n1 n1 </Note>
         <Note variant="N2">N2 N2 N2 n2 n2 </Note>
+      </Typography>
+      <Typography title="Paragraph">
+        <P>P1 P1 P1 p1 p1 </P>
+        <P variant="P2">P2 P2 P2 p2 p2 </P>
+        <P variant="P3">P3 P3 P3 p3 p3 </P>
       </Typography>
     </div>
   );
