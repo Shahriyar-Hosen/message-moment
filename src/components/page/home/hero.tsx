f12ddd1980b449039343277c629ea5c759e72274
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-05-30 14:31:31 +0600

    done

--- src/components/page/home/hero.tsx
+++ src/components/page/home/hero.tsx
@@ -1,12 +1,21 @@
 import { Container } from "@/components/common";
-import { SubTitle, Title } from "@/components/typography";
+import { P, SubTitle, Title } from "@/components/typography";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export const Hero = () => {
   return (
     <section>
       <Container
         customClass="bg-gradient-lite text-white"
-        className="py-[50px]"
+        className="py-[50px] relative"
       >
         <div className="h-[700px] bg-hero-image rounded-[20px] flex flex-col justify-center items-center gap-5 text-center pt-5">
           <Title className="max-w-[647px]">The real meaning to personal</Title>
@@ -15,7 +24,67 @@ export const Hero = () => {
             between known parties
           </SubTitle>
         </div>
+        <ChatBox />
       </Container>
     </section>
+  );
+};
+
+const ChatBox = () => {
+  return (
+    <div className="absolute -bottom-[62px] w-full max-w-[1320px]">
+      <div className="bg-primary text-center w-[1096px] h-[300px] rounded-[20px] mx-auto overflow-hidden">
+        <P variant="P3" className="text-white pb-7 px-[30px] pt-[28px]">
+          Click the button below to receive your random link that you can share
+          with your friends any way you like!
+        </P>
+        <div className="w-full h-full bg-black/[0.08] px-[30px] pt-[35px] pb-[25px] text-black">
+          <Select defaultValue="standard">
+            <SelectTrigger className="w-[140px] h-[50px] rounded-none rounded-s-[5px] outline-none">
+              <SelectValue placeholder="Select a fruit" />
+            </SelectTrigger>
+            <SelectContent className="rounded-none rounded-b-[5px] -mt-2 z-50">
+              <SelectGroup>
+                <SelectItem value="standard">
+                  <P className="flex justify-center items-center gap-2 text-dark">
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M11.488 9.6C11.552 9.072 11.6 8.544 11.6 8C11.6 7.456 11.552 6.928 11.488 6.4H14.192C14.32 6.912 14.4 7.448 14.4 8C14.4 8.552 14.32 9.088 14.192 9.6M10.072 14.048C10.552 13.16 10.92 12.2 11.176 11.2H13.536C12.768 12.52 11.544 13.544 10.072 14.048ZM9.872 9.6H6.128C6.048 9.072 6 8.544 6 8C6 7.456 6.048 6.92 6.128 6.4H9.872C9.944 6.92 10 7.456 10 8C10 8.544 9.944 9.072 9.872 9.6ZM8 14.368C7.336 13.408 6.8 12.344 6.472 11.2H9.528C9.2 12.344 8.664 13.408 8 14.368ZM4.8 4.8H2.464C3.224 3.472 4.456 2.448 5.92 1.952C5.44 2.84 5.08 3.8 4.8 4.8ZM2.464 11.2H4.8C5.08 12.2 5.44 13.16 5.92 14.048C4.456 13.544 3.224 12.52 2.464 11.2ZM1.808 9.6C1.68 9.088 1.6 8.552 1.6 8C1.6 7.448 1.68 6.912 1.808 6.4H4.512C4.448 6.928 4.4 7.456 4.4 8C4.4 8.544 4.448 9.072 4.512 9.6M8 1.624C8.664 2.584 9.2 3.656 9.528 4.8H6.472C6.8 3.656 7.336 2.584 8 1.624ZM13.536 4.8H11.176C10.92 3.8 10.552 2.84 10.072 1.952C11.544 2.456 12.768 3.472 13.536 4.8ZM8 0C3.576 0 0 3.6 0 8C0 10.1217 0.842855 12.1566 2.34315 13.6569C3.08601 14.3997 3.96793 14.989 4.93853 15.391C5.90914 15.7931 6.94943 16 8 16C10.1217 16 12.1566 15.1571 13.6569 13.6569C15.1571 12.1566 16 10.1217 16 8C16 6.94943 15.7931 5.90914 15.391 4.93853C14.989 3.96793 14.3997 3.08601 13.6569 2.34315C12.914 1.60028 12.0321 1.011 11.0615 0.608964C10.0909 0.206926 9.05058 0 8 0Z"
+                        fill="#CCCCCC"
+                      />
+                    </svg>
+                    <span>Standard</span>
+                  </P>
+                </SelectItem>
+                <SelectSeparator />
+                <SelectItem value="secure">
+                  <P className="flex justify-center items-center gap-2 text-dark">
+                    <svg
+                      width="13"
+                      height="16"
+                      viewBox="0 0 13 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M1.52381 16C1.10476 16 0.745906 15.8507 0.447239 15.552C0.148573 15.2533 -0.000506643 14.8947 1.29356e-06 14.4762V6.85714C1.29356e-06 6.4381 0.149334 6.07924 0.448001 5.78057C0.746668 5.4819 1.10527 5.33283 1.52381 5.33333H2.28572V3.80952C2.28572 2.75556 2.65727 1.85702 3.40038 1.11391C4.14349 0.370794 5.04178 -0.000507416 6.09524 5.20427e-07C7.14921 5.20427e-07 8.04775 0.371556 8.79086 1.11467C9.53397 1.85778 9.90527 2.75606 9.90476 3.80952V5.33333H10.6667C11.0857 5.33333 11.4446 5.48267 11.7432 5.78133C12.0419 6.08 12.191 6.4386 12.1905 6.85714V14.4762C12.1905 14.8952 12.0411 15.2541 11.7425 15.5528C11.4438 15.8514 11.0852 16.0005 10.6667 16H1.52381ZM6.09524 12.1905C6.51429 12.1905 6.87314 12.0411 7.17181 11.7425C7.47048 11.4438 7.61956 11.0852 7.61905 10.6667C7.61905 10.2476 7.46972 9.88876 7.17105 9.5901C6.87238 9.29143 6.51378 9.14235 6.09524 9.14286C5.67619 9.14286 5.31733 9.29219 5.01867 9.59086C4.72 9.88952 4.57092 10.2481 4.57143 10.6667C4.57143 11.0857 4.72076 11.4446 5.01943 11.7432C5.3181 12.0419 5.6767 12.191 6.09524 12.1905ZM3.80953 5.33333H8.38095V3.80952C8.38095 3.1746 8.15873 2.63492 7.71429 2.19048C7.26984 1.74603 6.73016 1.52381 6.09524 1.52381C5.46032 1.52381 4.92064 1.74603 4.47619 2.19048C4.03175 2.63492 3.80953 3.1746 3.80953 3.80952V5.33333Z"
+                        fill="#CCCCCC"
+                      />
+                    </svg>
+                    <span>Secure</span>
+                  </P>
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+    </div>
   );
 };
